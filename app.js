6690ef4f5b413b1abe9acd0ac96fe2f0cad537ea
David Harms <davidharms@MacBook-Pro.local>
2016-03-25 14:09:36 -0400

    first

--- app.js
+++ app.js
@@ -1,28 +1,11 @@
-angular.module('App', [])
-  .controller('CustomerListController', function() {
-    var customerList = this;
-    customerList.customers = [
-      {text:'learn angular', done:true},
-      {text:'build an angular app', done:false}];
 
-    customerList.addCustomer = function() {
-      customerList.customer.push({text:customerList.todoText, done:false});
-      customerList.todoText = '';
-    };
 
-    customerList.remaining = function() {
-      var count = 0;
-      angular.forEach(customerList.customer, function(todo) {
-        count += todo.done ? 0 : 1;
-      });
-      return count;
-    };
 
-    customerList.archive = function() {
-      var oldcustomer = customerList.customer;
-      customerList.customer = [];
-      angular.forEach(oldcustomer, function(todo) {
-        if (!todo.done) customerList.customer.push(todo);
-      });
-    };
-  });
+  var app = angular.module('app', [ ]);
+  app.controller('CustomerListController',['$http', function($http){
+    var list = this;
+    list.customers = [ ];
+    $http.get('/customers.json').success(function(data){
+      list.customers = data;
+    });
+  }]);
